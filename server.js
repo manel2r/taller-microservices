701246167aa0a9067d06c72568869df6f9f4c641
manel2r <manel.royo@kriter.net>
2018-03-13 08:55:53 +0100

    Routing with parameters

--- server.js
+++ server.js
@@ -5,19 +5,27 @@ app.get('/', (req, res) => res.send('Hola Món!'))
 
 
 app.get('/tasques', function (req, res) {
-  res.send('Preparat per a enviar tasques')
+  res.send('Preparat per a enviar totes les tasques')
+})
+
+app.get('/tasques/:userId', function (req, res) {
+  res.send('Preparat per a enviar les tasques del usuari' + req.params.userId)
+})
+
+app.get('/tasques/:userId/:tascaId', function (req, res) {
+  res.send('Preparat per a enviar la tasca ' + req.params.tascaId + ' del usuari ' + req.params.userId)
 })
 
 app.post('/tasques', function (req, res) {
   res.send('Preparat per a insertar una tasca')
 })
 
-app.put('/tasques', function (req, res) {
-  res.send('Preparat per a modificar una tasca')
+app.put('/tasques/:tascaID', function (req, res) {
+  res.send('Preparat per a modificar la tasca '+ request.params.tascaId)
 })
 
-app.delete('/tasques', function (req, res) {
-  res.send('Preparat per a esborrar una tasca')
+app.delete('/tasques/tascaId', function (req, res) {
+  res.send('Preparat per a esborrar la tasca ' + request.params.tascaId)
 })
 
 
